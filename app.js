9b9329aa8bf081b790ec4090e3de3a7177468f5c
Jaime Jacobo <jaimejacobordv@gmail.com>
2021-05-18 18:16:53 +0200

    basic back created

---
+++ app.js
@@ -0,0 +1,102 @@
+require('dotenv').config()
+
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const chalk = require('chalk')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+// -------- PORT --------
+const PORT = process.env.PORT || 5000
+
+// -------- MODELS --------
+const User = require('./models/User.models')
+
+// -------- MONGOOSE --------
+require('./configs/mongoose')
+
+const app = express()
+
+// -------- Middleware setup --------
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// -------- CORS --------
+
+app.use(
+  cors({
+    methods: ['GET', 'POST'],
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  })
+)
+
+// -------- PASSPORT --------
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+)
+
+passport.serializeUser((user, callback) => {
+  callback(null, user._id)
+})
+
+passport.deserializeUser((id, callback) => {
+  User.findById(id)
+    .then((result) => {
+      callback(null, result)
+    })
+    .catch((err) => {
+      callback(err)
+    })
+})
+
+app.use(flash())
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: `username`,
+      passwordField: `password`,
+      passReqToCallback: true,
+    },
+    (req, username, password, next) => {
+      User.findOne({ username })
+        .then((user) => {
+          if (!user) {
+            return next(null, false, {
+              message: `Incorrect username or password`,
+            })
+          }
+          if (!bcrypt.compareSync(password, user.password)) {
+            return next(null, false, {
+              message: `Incorrect username or password`,
+            })
+          }
+          return next(null, user)
+        })
+        .catch((err) => {
+          next(err)
+        })
+    }
+  )
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/', require('./routes/index.routes'))
+app.use('/auth', require('./routes/auth.routes'))
+
+app.listen(PORT, () => {
+  console.log(chalk.green.inverse(`Puerto activado en ${PORT}`))
+})
---
+++ routes/auth.routes.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const router = express.Router()
+
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+const User = require('../models/User.models')
+
+const checkForAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+/* Create New User */
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body
+  if (username === '' || password === '') {
+    res.send({ message: "Username and password can't be empty" })
+    return
+  } else if (password.length < 6) {
+    res.send({ message: 'The password must be at least 6 digits long' })
+    return
+  }
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        res.send({ message: 'This user already exists' })
+        return
+      } else {
+        const hashedPassword = bcrypt.hashSync(password, 10)
+        User.create({ username, password: hashedPassword }).then((result) => {
+          res.send({ message: 'User created', result })
+        })
+      }
+    })
+    .catch((err) => {
+      res.send({ message: err })
+    })
+})
+
+/* LOG IN */
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, failureDetails) => {
+    if (err) {
+      console.log(err)
+      res.send({ message: 'Something went bad with Passport Authentication' })
+      return
+    }
+
+    if (!user) {
+      res.send({ message: 'This user does not exist', failureDetails })
+      return
+    }
+
+    // res.cookie('sameSite', 'none', {
+    //   sameSite: true,
+    //   secure: true,
+    // })
+
+    req.login(user, (err) => {
+      if (err) {
+        res.send({ message: 'Something went bad with req.login', err })
+      } else {
+        User.findById(user._id)
+          .then((result) => {
+            res.send({ message: 'Log in succesful', result })
+          })
+          .catch(() => {
+            res.send({ message: 'Error finding the user' })
+          })
+      }
+    })
+  })(req, res, next)
+})
+
+router.get('/loggedin', (req, res, next) => {
+  res.send(req.user)
+})
+
+module.exports = router
